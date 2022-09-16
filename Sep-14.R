870d3510673755d4d17ea632c1a511c4777164d6
Pawar <pawar1550@gmail.com>
2022-09-16 14:46:20 -0400

    Add files via upload

--- Sep-14.R
+++ Sep-14.R
@@ -12,6 +12,13 @@ anova_one_way <- aov(Confirmed~Deaths, data = data)
 
 summary(anova_one_way) 
 
+# Post-hoc analysis!
+TukeyHSD(anova_one_way)
+
+# Function to find groups in your independent variable
+levels(data$gender)
+
+
 
 
 

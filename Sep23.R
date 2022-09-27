ee81df9be31028d2718544a3b3c9db21d981e9b1
Pawar <pawar1550@gmail.com>
2022-09-26 20:31:12 -0400

    Add files via upload

--- Sep23.R
+++ Sep23.R
@@ -10,4 +10,13 @@ pts = seq(-4.5,4.5,length=100)
 # Plots x and y to visualize distribution curve
 plot(pts,dt(pts,df=9),col='red',type='l')
 
+# check the distribution of x and y variables
+lines(density(x), col='green')
+lines(density(y), col='blue')
+
+
+
+
+
+
 
